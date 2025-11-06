907c13cbad239b8fa95c4730d1d666be31641992
misakazip <git@misaka.zip>
2025-11-06 21:15:38 +0900

    Reapply "lto: enable O3 optimizations on explicit lto modules"
    I accidentally reverted it. Sorry.
    This reverts commit 1ce0239ca95b241f8fb4b6a6b1f469554448b8fc.
    
    Change-Id: I6832850f40f0f0acb34a18a3dea1f2b9ebe7e882

--- cc/lto.go
+++ cc/lto.go
@@ -108,11 +108,15 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 	}
 	if lto.Properties.LtoEnabled {
 		ltoCFlags := []string{"-flto=thin", "-fsplit-lto-unit"}
+		var ltoCOnlyFlags []string
 		var ltoLdFlags []string
 
 		// Do not perform costly LTO optimizations for Eng builds.
 		if Bool(lto.Properties.Lto_O0) || ctx.Config().Eng() {
 			ltoLdFlags = append(ltoLdFlags, "-Wl,--lto-O0")
+		} else {
+			ltoLdFlags = append(ltoLdFlags,"-Wl,--lto-O3")
+			ltoCOnlyFlags = append(ltoCOnlyFlags, "-O3")
 		}
 
 		if Bool(lto.Properties.Whole_program_vtables) {
@@ -158,6 +162,7 @@ func (lto *lto) flags(ctx ModuleContext, flags Flags) Flags {
 		flags.Local.AsFlags = append(flags.Local.AsFlags, ltoCFlags...)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoCFlags...)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoLdFlags...)
+		flags.Local.CFlags = append(flags.Local.CFlags, ltoCOnlyFlags...)
 	}
 	return flags
 }
